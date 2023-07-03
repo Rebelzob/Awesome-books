d3a78ba4c0d478a7e09486d9ea9ae99114c56e0b
Rebelzob <karimdavid07@gmail.com>
2023-07-03 18:17:17 +0100

    Added HTML structure and started variables in JS file

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const title = document.getElementById('title-input');
+const author = document.getElementById('author-input');
+const booksList = document.getElementById('books-list');
+const addButton = document.getElementById('add-btn')
+const removeButton = document.getElementById('remove-btn');
+
+
+const books = {}
