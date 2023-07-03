7c8a46a06f109282eaddbc731ac32de6bc55a9c0
Asiimwe Mark <markasiimwe75@gmail.com>
2023-07-03 20:47:58 +0300

    Added dynamic Html in Js file

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,27 @@ const addButton = document.getElementById('add-btn')
 const removeButton = document.getElementById('remove-btn');
 
 
-const books = {}
+const books = [];
+form.addEventListener('submit',function(e) {
+  const book = {
+    title :title.value,
+    author : author.value
+}
+books.push(book);
+
+  books.forEach((book)=>{
+    let bookHtml='';
+    bookHtml+=`<article id="listed-books">
+    <ul>
+        <li>${book.title}</li>
+        <li>${book.author}</li>
+        <li><button type="button" id="remove-btn">Remove</button></li>
+    </ul>
+</article>`
+booksList.innerHTML=bookHtml;
+  })
+  e.preventDefault();
+})
+
+// console.log(books)
+ 
