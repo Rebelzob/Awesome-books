96ef684ed873c5e2e04c070e46ab0bf115937956
Rebelzob <karimdavid07@gmail.com>
2023-07-03 20:27:04 +0100

    Added javascript logic to the book list

--- src/js/script.js
+++ src/js/script.js
@@ -7,26 +7,28 @@ const removeButton = document.getElementById('remove-btn');
 
 
 const books = [];
+
 form.addEventListener('submit',function(e) {
+  e.preventDefault();
   const book = {
     title :title.value,
     author : author.value
-}
+  }
 books.push(book);
-
-  books.forEach((book)=>{
-    let bookHtml='';
-    bookHtml+=`<article id="listed-books">
-    <ul>
-        <li>${book.title}</li>
-        <li>${book.author}</li>
-        <li><button type="button" id="remove-btn">Remove</button></li>
-    </ul>
-</article>`
-booksList.innerHTML=bookHtml;
-  })
-  e.preventDefault();
+createBook(book);
 })
 
-// console.log(books)
- 
+console.log(books)
+
+function createBook(book) {
+  const newBook = document.createElement('article');
+  newBook.innerHTML = 
+  `<div id="listed-books">
+      <ul>
+          <li>${book.title}</li>
+          <li>${book.author}</li>
+          <li><button type="button" id="remove-btn">Remove</button></li>
+      </ul>
+  </div>`;
+  booksList.appendChild(newBook)
+}
