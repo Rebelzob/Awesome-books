43c323a343bf3ea6f4c7853d8193eebc5d16de8e
Asiimwe Mark <markasiimwe75@gmail.com>
2023-07-04 15:49:39 +0300

    Added clear field functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,13 @@
 const form = document.getElementById('form');
 const title = document.getElementById('title-input');
 const author = document.getElementById('author-input');
-const booksList = document.getElementById('books-list');
+const booksList = document.getElementById('books');
 const addButton = document.getElementById('add-btn')
-const removeButton = document.getElementById('remove-btn');
+const removeButton = document.querySelector('.remove-btn');
 
 
 let books = [];
-
+let id=0;
 
 
 form.addEventListener('submit',function(e) {
@@ -22,31 +22,56 @@ createBook(book);
 clearFields();
 })
 
+
+
 function createBook(book) {
-  const newBook = document.createElement('article');
+  const newBook = document.createElement('li');
   newBook.innerHTML = 
-  `<div id="listed-books">
-      <ul>
-          <li>${book.title}</li>
-          <li>${book.author}</li>
-          <li><button type="button" id="${book.id}">Remove</button></li>
-      </ul>
-  </div>`;
+  `
+     <div>${book.title}</div>
+      <div>${book.author}</div>
+      <button type="button" class="remove-btn" id="${book.id}">Remove</button>
+        <hr>
+  `
+
   booksList.appendChild(newBook);
   }
-function Delete(){
-  removeButton.addEventListener('click',(e)=>{
-    books=books.filter((book=>id=e.target));
-  }) 
-}
-
-Delete();
- 
 
 function clearFields(){
   const title = document.getElementById('title-input').value='';
   const author = document.getElementById('author-input').value='';
 }
+
+removeButton.addEventListener('click',function(e){
+  if(e.target.classList.contains('remove-btn')){
+    const id=e.target.id;
+
+    removeBook(id);
+  }
+})
+
+console.log(booksList)
+
+function removeBook(id){
+  const index=books.findIndex(book=>book.id===id)
+  if(index !==-1){
+    books.splice(index, 1)
+    // createBook(book);
+
+    function createBook(book) {
+      const newBook = document.createElement('li');
+      newBook.innerHTML = 
+      `
+         <div>${book.title}</div>
+          <div>${book.author}</div>
+          <button type="button" class="remove-btn" id="${book.id}">Remove</button>
+            <hr>
+      `
+      booksList.appendChild(newBook);
+      }
+  }
+}
+
 
 
 
