d49859d60ad721985760d4c63dc38747365e9783
Asiimwe Mark <markasiimwe75@gmail.com>
2023-07-06 00:15:01 +0300

    Added navigation and navigation functionality

--- src/js/script.js
+++ src/js/script.js
@@ -91,18 +91,27 @@ btnList.addEventListener('click',function(){
   bookList.classList.add('display')
   addBook_.classList.remove('display')
   contact.classList.remove('display')
+  btnList.style.color='blue'
+  btnAdd.style.color='black'
+  btnContact.style.color='black'
 })
 
 btnAdd.addEventListener('click',function(){
   bookList.classList.remove('display')
   addBook_.classList.add('display')
   contact.classList.remove('display')
+  btnList.style.color='black'
+  btnAdd.style.color='blue'
+  btnContact.style.color='black'
 })
 
 btnContact.addEventListener('click',function(){
   bookList.classList.remove('display')
   addBook_.classList.remove('display')
   contact.classList.add('display')
+  btnList.style.color='black'
+  btnAdd.style.color='black'
+  btnContact.style.color='blue'
 })
 
 
